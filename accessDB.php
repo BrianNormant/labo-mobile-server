56661ec12630581999e6ea4e479b8b2ddc78f18b
BrianNixDesktop <briannormant@gmail.com>
2024-04-09 13:58:54 -0400

    create starter routes

---
+++ accessDB.php
@@ -0,0 +1,2 @@
+<?php
+$dbh = new PDO('mysql:host=database:3306;dbname=docker', 'docker', 'docker');
--- routes.php
+++ routes.php
@@ -2,4 +2,8 @@
 
 require_once __DIR__.'/router.php';
 
+get('/couleursDisponibles',  'colors.php');
+get('/codesSecrets',         'codes.php');
+get('/stats',                'stats.php');
+
 any('/404','views/404.php');
