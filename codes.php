e93d0d0d7782db7cc8d6bd3634e840467210c8f0
BrianNixDesktop <briannormant@gmail.com>
2024-04-09 15:46:41 -0400

    endpoint colors

---
+++ codes.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__.'/accessDB.php'
+
+if (isset($_REQUEST["nbCouleurs"]))
+	$nb_col = $_REQUEST["nbCouleurs"];
+
+
--- colors.php
+++ colors.php
@@ -3,3 +3,8 @@
 require_once __DIR__.'/accessDB.php';
 
 $sth = $dbh->query("SELECT * FROM Color;");
+
+$colors = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+header('Content-Type: application/json');
+echo json_encode($colors);
