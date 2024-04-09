6ee3f723cf3665459aa813eebff408b945964d73
BrianNixDesktop <briannormant@gmail.com>
2024-04-09 16:18:13 -0400

    codes endpoints functional

--- codes.php
+++ codes.php
@@ -1,8 +1,37 @@
 <?php
 
-require_once __DIR__.'/accessDB.php'
+require_once __DIR__.'/accessDB.php';
 
-if (isset($_REQUEST["nbCouleurs"]))
+function r_fuc($ac, $r) {
+	if (!isset($ac[$r["secret"]])) {
+		$ac[$r["secret"]] = array(
+			"id" => $r["secret"],
+			"code" => array($r["color"]),
+			"nbCouleurs" => $r["nb_colors"],
+		);
+	} else {
+		$ac[$r["secret"]]["code"] []= $r["color"];
+	}
+	return $ac;
+}
+
+if (isset($_REQUEST["nbCouleurs"])) {
 	$nb_col = $_REQUEST["nbCouleurs"];
 
+	$sth = $dbh->prepare(<<<SQL
+	SELECT * FROM Code WHERE nb_colors = ?
+	ORDER BY secret, position;
+	SQL);
+	$sth->execute([$nb_col]);
+} else {
+	$sth = $dbh->query(<<<SQL
+	SELECT * FROM Code ORDER BY secret, position;
+	SQL);
+}
 
+
+$data = $sth->fetchAll(PDO::FETCH_ASSOC);
+$data = array_reduce($data, "r_fuc", []);
+
+header("Content-Type: application/json");
+echo json_encode($data);
