30e85eef4cdb53c825654873d7acaf0e8f11d179
BrianNixDesktop <briannormant@gmail.com>
2024-04-09 16:46:56 -0400

    add stat update/create endpoint

--- routes.php
+++ routes.php
@@ -7,5 +7,6 @@ get('/colors',  'colors.php');
 get('/codesSecrets',         'codes.php');
 get('/codes',         'codes.php');
 get('/stats',                'stats.php');
+put('/stats',                'update_stat.php');
 
 any('/404','views/404.php');
---
+++ update_stat.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once __DIR__.'/accessDB.php';
+
+$body = file_get_contents('php://input');
+$body = json_decode($body, true);
+
+if (!isset($body["idCode"]) 
+	|| !isset($body["record"])
+	|| !isset($body["email"])) {
+	http_response_code(417);
+	exit;
+}
+
+$sth = $dbh->prepare("SELECT * FROM Code WHERE secret = ?;");
+$sth->execute([$body["idCode"]]);
+if ($sth->rowCount() == 0) {
+	http_response_code(404);
+	exit;
+}
+
+
+$sth = $dbh->prepare("SELECT * FROM Stat WHERE secret = ?;");
+$sth->execute([$body["idCode"]]);
+if ($sth->rowCount() == 0) {
+	$sth = $dbh->prepare(<<<SQL
+	INSERT INTO Stat (secret, nb_colors, record, email) VALUES (
+		:secret,
+		(SELECT nb_colors FROM Code WHERE secret = :secret LIMIT 1),
+		:record,
+		:email
+	);
+	SQL);
+} else {
+	$sth = $dbh->prepare(<<<SQL
+	UPDATE Stat 
+	SET record = :record,
+		email  = :email
+	WHERE secret = :secret AND :record > record;
+	SQL);
+}
+
+$sth->execute([
+	"record" => $body["record"],
+	"email"  => $body["email"],
+	"secret" => $body["idCode"]
+]);
